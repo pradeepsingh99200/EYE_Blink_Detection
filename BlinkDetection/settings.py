60b2f4fd4c761be74cb17542ce691eb3d2da7a07
Pradeep Singh <119406692+pradeepsingh99200@users.noreply.github.com>
2024-09-09 13:08:14 +0530

    Update settings.py

--- BlinkDetection/settings.py
+++ BlinkDetection/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5-)za)i4a%ev4s!k$s3pph%@pdgq&ridwq+z=3yfeop=d3a)st'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
